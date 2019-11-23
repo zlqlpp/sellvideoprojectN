9b8588a87a7d95fe8882af885162855fada39ea0
zlqlpp <zlqlpp@gmail.com>
2019-11-23 13:06:47 +0800

    code

--- src/main/java/rml/controller/ManageController.java
+++ src/main/java/rml/controller/ManageController.java
@@ -338,7 +338,7 @@ class MusicImplements implements Runnable{
         
         processList = new ArrayList<String>();
 		try {
-			Logger.getLogger(MusicImplements.class).info("cd "+p.getProperty("videoPath")+";ls "+id+"*"); 
+			Logger.getLogger(MusicImplements.class).info("cd "+p.getProperty("videoPath")+";ls "+id+"\\*"); 
 			Process pro = Runtime.getRuntime().exec("cd "+p.getProperty("videoPath")+";ls "+id+"*");
 			pro.waitFor();
 			BufferedReader input = new BufferedReader(new InputStreamReader(pro.getInputStream()));
